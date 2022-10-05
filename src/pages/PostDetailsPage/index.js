af6d42d6e446c0c9641e4c53bf893b849acbd2a4
Mauro-Molina <tutowebgoo@gmail.com>
2022-10-05 14:38:14 -0400

    Add styles details post page

--- src/pages/PostDetailsPage/index.js
+++ src/pages/PostDetailsPage/index.js
@@ -1,6 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  VisuallyHidden,
+  List,
+  ListItem,
+} from '@chakra-ui/react';
  
 const API_URL = "http://localhost:5005";       
 
@@ -29,19 +47,128 @@ function PostDetailsPage (props) {
     <div className="">
       {post && (
         <>
-          <h1>{post.name}</h1>
-          <p>{post.description}</p>
+        <Container maxW={'7xl'}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Image
+            rounded={'md'}
+            alt={'product image'}
+            src={
+              post.imageUrl
+            }
+            fit={'cover'}
+            align={'center'}
+            w={'100%'}
+            h={{ base: '100%', sm: '400px', lg: '500px' }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={'header'}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+             {post.name}
+            </Heading>
+           
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue('gray.200', 'gray.600')}
+              />
+            }>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                color={useColorModeValue('gray.500', 'gray.400')}
+                fontSize={'2xl'}
+                fontWeight={'300'}>
+                {post.description}
+           </Text>
+            </VStack>
+            <Box>
+              <Text
+                fontSize={{ base: '16px', lg: '18px' }}
+                color={useColorModeValue('yellow.500', 'yellow.300')}
+                fontWeight={'500'}
+                textTransform={'uppercase'}
+                mb={'4'}>
+                Features
+              </Text>
+
+              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <List spacing={2}>
+                  <ListItem>Chronograph</ListItem>
+                  <ListItem>Master Chronometer Certified</ListItem>{' '}
+                  <ListItem>Tachymeter</ListItem>
+                </List>
+                <List spacing={2}>
+                  <ListItem>Anti‑magnetic</ListItem>
+                  <ListItem>Chronometer</ListItem>
+                  <ListItem>Small seconds</ListItem>
+                </List>
+              </SimpleGrid>
+            </Box>
+            <Box>
+            
+          
+            </Box>
+          </Stack>
+
+
+          <Stack  alignItems="center" justifyContent={'center'} className="flex-btn">
+          
+          <Button
+           as={'a'}
+           href={"/post"}
+            rounded={'none'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+           Back to Post
+          </Button>
+
+          
+          <Button
+           as={'a'}
+           href={`/post/edit/${postId}`}
+            rounded={'none'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+            Add to cart
+          </Button>
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
           
         </>
       )}
 
-      <Link to="/post">
-        <button>Back to projects</button>
-      </Link>
- <br/>
-      <Link to={`/post/edit/${postId}`}>
-        <button>Edit Project</button>
-      </Link>      
+   
     </div>
   );
 }
