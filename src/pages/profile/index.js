9d8269f2a0a742d2cdabf1fbd5847d360dd6cc47
Mauro-Molina <tutowebgoo@gmail.com>
2022-10-06 16:48:20 -0400

    Add icon

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -12,9 +12,10 @@ const Profile = (props) => {
                     size={'lg'}
                     src={props.user.imageUrl}
                   />
-            <h5 className='user-name'><strong>User: </strong>{props.user.username}</h5>
-          <h5 className='user-occupation'><strong>Ocupation: </strong>{props.user.ocupation}</h5>
-          <h5 className='user-occupation'><strong>Email: </strong>{props.user.email}</h5>
+                  <br></br><br></br>
+            <h5 className='user-name'><i class="bi bi-person"></i> <strong>User: </strong>{props.user.username}</h5>
+          <h5 className='user-occupation'><i class="bi bi-terminal"></i> <strong>Ocupation: </strong>{props.user.ocupation}</h5>
+          <h5 className='user-occupation'><i class="bi bi-envelope"></i> <strong>Email: </strong>{props.user.email}</h5>
         </div>
        
        
